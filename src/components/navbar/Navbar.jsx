82780c73f12e11e21e9db5b88c8fc6dd99a84993
Hudson Alves <hudsonalvesfl@gmail.com>
2023-05-27 16:25:59 -0300

    adicionado o componente menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import styles from "../../styles/navbar/navbar.module.css"
 import { BackgroundMiniLogo } from "../img/BackgroundMiniLogo"
+import { Menu } from "./navbar_components/Menu"
 
 export function Navbar(){
     return (
         <nav id={styles.navbar}>
             <BackgroundMiniLogo />
+            <ul id={styles.menu}>
+                <Menu rotina="Cadastro"/>
+                <Menu rotina="Caixa"/>
+                <Menu rotina="Notas fiscais"/>
+            </ul>
         </nav>
     )
 }
---
+++ src/components/navbar/navbar_components/Menu.jsx
@@ -0,0 +1,5 @@
+export function Menu({rotina}){
+    return (
+        <li>{rotina}</li>
+    )
+}
